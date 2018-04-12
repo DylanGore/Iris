adc69c1cef4a4773ecc267c34922202e299a9066
Dylan Gore <hello@dylangore.ie>
2018-04-12 14:36:51 +0100

    Added missing file

---
+++ src/pi-net/pi-net.py
@@ -0,0 +1,16 @@
+#! /usr/local/bin/python3
+
+import netifaces
+from pushbullet import Pushbullet
+
+pb = Pushbullet("PUSHBULLET_API_KEY")
+
+# Get IP address
+localip = netifaces.ifaddresses('wlan0')[netifaces.AF_INET][0]['addr']
+
+# Send push
+user = pb.chats[0]
+user.push_note("Raspberry Pi IP Address", localip)
+
+# Uncomment the line below to send the push to yourself rather than someone else
+#pb.push_note("Raspberry Pi IP", localip)
