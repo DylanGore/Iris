e25568613dcaeea5c0caa7afdef8f4edeb592505
Dylan Gore <hello@dylangore.ie>
2018-04-16 16:44:48 +0100

    Devices are now user-specific

--- src/web/devices/views.py
+++ src/web/devices/views.py
@@ -10,7 +10,12 @@ def index(request):
 
 # Devices List View - Manage
 def manage_devices(request):
-    all_cameras = Camera.objects.all()
+    # Show all cameras if user is an admin, otherwise show only devices belonging to a specific user
+    if request.user.is_staff:
+        all_cameras = Camera.objects.all()
+    else:
+        all_cameras = Camera.objects.filter(owner_name=request.user.username)
+
     if request.user.is_authenticated:
         return render(request, 'devices/manage_devices.html', {'all_cameras': all_cameras})
     else:
@@ -19,7 +24,12 @@ def manage_devices(request):
 
 # Devices List View - Monitoring
 def monitor_devices(request):
-    all_cameras = Camera.objects.all
+    # Show all cameras if user is an admin, otherwise show only devices belonging to a specific user
+    if request.user.is_staff:
+        all_cameras = Camera.objects.all()
+    else:
+        all_cameras = Camera.objects.filter(owner_name=request.user.username)
+
     if request.user.is_authenticated:
         return render(request, 'devices/monitor_devices.html', {'all_cameras': all_cameras})
     else:
@@ -29,7 +39,7 @@ def monitor_devices(request):
 # Specific Camera Info
 def manage_camera(request, device_id):
     device = get_object_or_404(Camera, id=device_id)
-    if request.user.is_authenticated:
+    if request.user.is_authenticated and (device.owner_name == request.user.username or request.user.is_staff):
         return render(request, 'devices/manage_camera.html', {'device': device})
     else:
         return render(request, 'users/unauthenticated.html')
@@ -38,7 +48,7 @@ def manage_camera(request, device_id):
 # View Camera Feed Full Screen
 def view_camera(request, device_id):
     device = get_object_or_404(Camera, id=device_id)
-    if request.user.is_authenticated:
+    if request.user.is_authenticated and (device.owner_name == request.user.username or request.user.is_staff):
         return render(request, 'devices/view_camera.html', {'device': device})
     else:
         return render(request, 'users/unauthenticated.html')
