905cb6528dca05e4baa6b3d30e14acfe32aff8c6
Dylan Gore <hello@dylangore.ie>
2018-04-19 20:23:56 +0100

    Added Pushbullet functionality until Telegram bot can be used

---
+++ src/camera-client/pb.py
@@ -0,0 +1,11 @@
+#! /usr/local/bin/python3
+
+from pushbullet import Pushbullet
+from variables import PUSHBULLET_API_KEY
+
+pb = Pushbullet(PUSHBULLET_API_KEY)
+
+# Send push
+def sendPush(title, message):
+    user = pb.chats[0]
+    user.push_note(title, message)
