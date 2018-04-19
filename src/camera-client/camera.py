8f99fe3d99b4cfb4afa28ba2ab98fb2de72fee77
Dylan Gore <hello@dylangore.ie>
2018-04-19 21:01:03 +0100

    Fixed camera code

--- src/camera-client/camera.py
+++ src/camera-client/camera.py
@@ -3,12 +3,16 @@
 from time import sleep
 from helper import * 
 import time
+import datetime
 import picamera
 
 def runCamera(run_time):
-    end_time = time.time() + run_time
+    curr_time = time.time()
+    end_time = curr_time + run_time
     cam = picamera.PiCamera()
+    timestamp = datetime.datetime.fromtimestamp(curr_time).strftime('%Y-%m-%d-%H:%M:%S')
     if time.time() < end_time:
         log('CAMERA - taking photos')
-        cam.capture('/home/dylan/iris_output/img-' + time + '.jpg')
-        sleep(1)
+        cam.capture('/home/dylan/iris_output/img-' + timestamp + '.jpg')
+        cam.close()
+        sleep(0.5)
